23e710f072f9ceca257eba822f67bb0abe15f92f
MoiChung-nwc <moichung2411@gmail.com>
2024-05-31 23:52:41 +0700

    update sub category, brands

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -110,7 +110,7 @@ $("#categoryForm").submit(function(event) {
                     $("#name").addClass('is-invalid').siblings('p').addClass('invalid-feedback').html(errors['name']);
                 } else {
                     $("#name").removeClass('is-invalid').siblings('p').removeClass('invalid-feedback').html("");
-                }
+                }g
 
                 if(errors['slug']) {
                     $("#slug").addClass('is-invalid').siblings('p').addClass('invalid-feedback').html(errors['slug']);
